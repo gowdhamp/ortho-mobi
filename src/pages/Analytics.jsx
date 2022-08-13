5757ea6cb76a77c655fe9658d70f6a06999b3b24
gowtham-surya <gowthamsurya95855@gmail.com>
2022-08-13 16:59:52 +0530

    Changes in Foot images

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -6,12 +6,12 @@ const Analytics = () => {
     <div>
       <h2 className="page-header">Analytics</h2>
       <div className="row">
-        <div className="col-6">
+        <div className="col-4">
           <div className="card">
             <h3>Pressure</h3>
             <div className="img-container">
               <img src={footprint} alt="footprint" />
-              <div className="pressure-point point-1 low"></div>
+              <div title="hello" className="pressure-point point-1 low"></div>
               <div className="pressure-point point-2 low"></div>
               <div className="pressure-point point-3 medium"></div>
               <div className="pressure-point point-4 high"></div>
